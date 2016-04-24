9d86c53b0f4341b9ae2b2e81a8decf77f4beea13
Toby Privett <toby@snaplab.co.uk>
2016-04-24 13:58:20 +0100

    Define the Fact Host and URI

--- lib/feline/cat.rb
+++ lib/feline/cat.rb
@@ -1,5 +1,7 @@
 module Feline
   class Cat
     RANDOM_IMAGE = "http://thecatapi.com/api/images/get?format=src&type=gif"
+    FACT_HOST = "catfacts-api.appspot.com"
+    FACT_URI = "/api/facts"
   end
 end
